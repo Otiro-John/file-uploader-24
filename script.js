74da338462d86ed2e5cbf71d06f220d0b2420a73
Otiro-John <126407500+Otiro-John@users.noreply.github.com>
2024-03-02 19:43:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+
+
+form.addEventListener("click", () => {
+    fileInput.click();
+
+});
+
+fileInput.onchange = ({target}) => {
+let file = target.files[0];
+if(file){
+    let fileName = file.name;
+    uploadFile(fileName);
+}
+
+}
+
+function uploadFile(name){
+    
+}
